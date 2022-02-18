b3560b6a5b6c74d79b1d5369e40c7c2c12f0a00f
1zong2 <1zong2@innerverz.com>
2022-02-19 07:24:20 +0900

    remove D2 and add lpips loss in SimSwapLoss

--- simswap/loss.py
+++ simswap/loss.py
@@ -8,14 +8,12 @@ class SimSwapLoss(LossInterface):
         self.start_time = time.time()
         self._loss_dict = {}
 
-    def get_loss_G(self, I_source, I_target, same_person, I_swapped, g_fake1, g_fake2, g_real1, g_real2, id_swapped, id_source):
+    def get_loss_G(self, I_source, I_target, same_person, I_swapped, g_fake, g_real, id_swapped, id_source):
         L_G = 0.0
         
         # Adversarial loss
         if self.args.W_adv:
-            L_adv = 0
-            L_adv += Loss.get_hinge_loss(g_fake1, True)
-            L_adv += Loss.get_hinge_loss(g_fake2, True)
+            L_adv = Loss.get_hinge_loss(g_fake, True)
             L_G += self.args.W_adv * L_adv
             self.loss_dict["L_adv"] = round(L_adv.item(), 4)
         
@@ -37,6 +35,12 @@ class SimSwapLoss(LossInterface):
             L_G += self.args.W_recon * L_recon
             self.loss_dict["L_recon"] = round(L_recon.item(), 4)
         
+        # LPIPS loss
+        if self.args.W_lpips:
+            L_lpips = Loss.get_lpips_loss(I_swapped, I_target)
+            L_G += self.args.W_lpips * L_lpips
+            self.loss_dict["L_lpips"] = round(L_lpips.item(), 4)
+        
         # Feature matching loss 
         if self.args.W_fm:
             L_fm = 0
@@ -45,8 +49,7 @@ class SimSwapLoss(LossInterface):
             feat_weights = 4.0 / (n_layers_D + 1)
             D_weights = 1.0 / num_D
             for i in range(0, n_layers_D):
-                L_fm += D_weights * feat_weights * Loss.get_L1_loss(g_fake1[i], g_real1[i].detach())
-                L_fm += D_weights * feat_weights * Loss.get_L1_loss(g_fake2[i], g_real2[i].detach())
+                L_fm += D_weights * feat_weights * Loss.get_L1_loss(g_fake[i], g_real[i].detach())
             L_G += self.args.W_fm * L_fm
             self.loss_dict["L_fm"] = round(L_recon.item(), 4)
 
@@ -54,16 +57,10 @@ class SimSwapLoss(LossInterface):
 
         return L_G
 
-    def get_loss_D(self, d_real1, d_real2, d_fake1, d_fake2):
+    def get_loss_D(self, d_real, d_fake):
         # Real 
-        L_D_real = 0
-        L_D_real += Loss.get_hinge_loss(d_real1, True)
-        L_D_real += Loss.get_hinge_loss(d_real2, True)
-
-        # Fake
-        L_D_fake = 0
-        L_D_fake += Loss.get_hinge_loss(d_fake1, False)
-        L_D_fake += Loss.get_hinge_loss(d_fake2, False)
+        L_D_real = Loss.get_hinge_loss(d_real, True)
+        L_D_fake = Loss.get_hinge_loss(d_fake, False)
 
         L_D = 0.5*(L_D_real.mean() + L_D_fake.mean())
         
